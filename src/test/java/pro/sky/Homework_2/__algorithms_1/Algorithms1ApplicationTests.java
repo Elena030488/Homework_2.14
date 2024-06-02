5041d63614dc929ca69bbf4d57e058ebcf283af2
Lenka <aruru.goddes@gmail.com>
2024-06-03 01:43:30 +0300

    Домашнее задание № 2.14

---
+++ src/test/java/pro/sky/Homework_2/__algorithms_1/Algorithms1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.Homework_2.__algorithms_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Algorithms1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
